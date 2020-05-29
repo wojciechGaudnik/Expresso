a1bb2271b79fff3543958b55192ca3fe4a17f56b
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2020-05-29 19:37:17 +0200

    init

---
+++ migration.js
@@ -0,0 +1,39 @@
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('database.sqlite');
+
+db.serialize(function () {
+    db.run(`drop table if exists Employee`);
+    db.run(`create table Employee
+            (
+                id                  integer not null primary key,
+                name                text    not null,
+                position            text    not null,
+                wage                integer not null,
+                is_current_employee integer default 1
+            );`);
+    db.run(`drop table if exists TimeSheet`);
+    db.run(`create table TimeSheet
+            (
+                id          integer not null primary key,
+                hours       integer not null,
+                rate        integer not null,
+                date        integer not null,
+                employee_id integer not null references Employee (id)
+            );`);
+    db.run(`drop table if exists Menu`);
+    db.run(`create table Menu
+            (
+                id    integer not null primary key,
+                title text    not null
+            );`);
+    db.run(`drop table if exists MenuItem`);
+    db.run(`create table MenuItem
+            (
+                id          integer not null primary key,
+                name        text    not null,
+                description text,
+                inventory   integer not null,
+                price       integer not null,
+                menu_id     integer not null references Menu (id)
+            );`);
+});
