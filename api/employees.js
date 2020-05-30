9d2a7fe71104badc20dc8d9905217940ca693398
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2020-05-30 15:20:09 +0200

    added GET/:employeeId

--- api/employees.js
+++ api/employees.js
@@ -5,6 +5,19 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || 'database.sqlite');
 console.log(process.env.TEST_DATABASE);
 
+employeesRouter.param('employeeId', ((req, res, next, employeeID) => {
+    db.get(`select * from Employee where id = $employeeId`, {$employeeId: employeeID}, (err, employee) => {
+        if (err) {
+            next(err);
+        } else if (employee) {
+            req.employee = employee;
+            next();
+        } else {
+            res.sendStatus(404);
+        }
+    })
+}))
+
 employeesRouter.get('/', (req, res, next) => {
     db.all(`select * from Employee where is_current_employee = 1`, (err, employees) => {
         if (err) {
@@ -15,5 +28,9 @@ employeesRouter.get('/', (req, res, next) => {
         }
     });
 })
+
+employeesRouter.get('/:employeeId', ((req, res, next) => {
+    res.status(200).json({employee: req.employee});
+}))
 
 module.exports = employeesRouter;
