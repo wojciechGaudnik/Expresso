5b804e8e395b06e380bd479089b876002b43d778
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2020-06-01 14:17:01 +0200

    added GET /api/menus/:id

--- api/menus.js
+++ api/menus.js
@@ -3,15 +3,36 @@ const menusRouter = express.Router();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || 'database.sqlite');
 
-menusRouter.get('/', ((req, res, next) => {
+menusRouter.param('menuId', (req, res, next, menuId) => {
+    db.get(`select *
+            from Menu
+            where id = $menuId`, {$menuId: menuId}, (err, menu) => {
+        if (err) {
+            next(err);
+        } else if (menu) {
+            req.menu = menu;
+            next();
+        } else {
+            return res.sendStatus(404);
+        }
+    });
+})
+
+
+menusRouter.get('/', (req, res, next) => {
     db.all(`select *
             from Menu`, (err, menus) => {
         if (err) {
-            nest(err);
+            next(err);
         } else {
             res.status(200).json({menus: menus});
         }
     });
-}));
+});
+
+menusRouter.get('/:menuId', (req, res) => {
+    res.status(200).json({menu: req.menu});
+})
+
 
 module.exports = menusRouter;
